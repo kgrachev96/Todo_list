c806c34e9979cb4a12e129e7d00fdb2f9f481a27
Kirill Grachev <kgrachev96@gmail.com>
2018-02-09 17:02:00 +0300

    update

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: [
-        // "webpack-dev-server/client?http://localhost:8080/",
+        "webpack-dev-server/client?http://localhost/",
         "./src/app.tsx"
     ],
 
